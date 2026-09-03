e04c76545f9202fc66d9a7546f7727d3ed0dc747
marxthoncr-commits <marxthoncr@gmail.com>
2026-09-03 15:54:24 -0500

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -5,8 +5,9 @@
 
 const path = require("path");
 const Database = require("better-sqlite3");
-
+const fs = require("fs")
 const DB_PATH = process.env.DATABASE_PATH || path.join(__dirname, "..", "data", "vinklo.db");
+fs.mkdirSync(path.dirname(DB_PATH), { recursive: true });
 const db = new Database(DB_PATH);
 db.pragma("journal_mode = WAL");
 db.pragma("foreign_keys = ON");
